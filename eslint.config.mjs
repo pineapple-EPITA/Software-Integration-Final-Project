2fd208bb268ddb02a8957f3b1c1928a28cf63e58
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-04-29 13:56:53 +0200

    chore: test husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,30 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {
+      'eslint-plugin-react': pluginReact, // Thêm plugin react
+      '@typescript-eslint': tseslint, // Thêm plugin TypeScript
+    },
+    extends: [
+      'plugin:@typescript-eslint/recommended', // Cấu hình chuẩn TypeScript
+      'plugin:react/recommended', // Cấu hình chuẩn React
+    ],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ]);
